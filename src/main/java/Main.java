4652bb8d8438944e48f05cc121102aa8a6ee3968
uladzimirAAsanko <volodyasn@gmail.com>
2021-05-17 13:27:53 +0300

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,15 @@ import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 import static org.apache.spark.sql.functions.col;
 
 public class Main {
+    private static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
     public static void main(String[] args) {
         SparkSession spark = SparkSession.builder().appName("Simple Application").getOrCreate();
         spark.sparkContext().setLogLevel("ERROR");
@@ -23,14 +25,43 @@ public class Main {
         }
         List<Long> hotelsID =  usersDF.selectExpr("CAST(hotel_id AS LONG)").as(Encoders.LONG()).collectAsList();//Long.parseLong(row.toString())) hotels_id.add(row.getLong(0)
         HashSet<Long> longs = new HashSet<>(hotelsID);
+        HashMap<Long,ArrayList<String>> listHashMap = new HashMap<>();
+        longs.forEach(s-> {
+            ArrayList<String> list = new ArrayList<>();
+            List<String> values = usersDF.selectExpr("CAST(srch_ci AS STRING)").
+                    where("hotel_id=" + s).
+                    orderBy("srch_ci").
+                    as(Encoders.STRING()).
+                    collectAsList();
+            String prevVal = values.get(0);
+            for(String data : values){
+                try {
+                    Date prevDate = format.parse(prevVal);
+                    Date currDate = format.parse(data);
+                    long diff = prevDate.getTime() - currDate.getTime();
+                    long dayDiff = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+                    if (dayDiff > 1){
+                        System.out.println("Hotel with missing date  is " + s);
+                        Calendar c = Calendar.getInstance();
+                        c.setTime(prevDate);
+                        c.add(Calendar.DATE,1);
+                        while(c.getTime().before(currDate)){
+                            String tmp = format.format(c.getTime());
+                            System.out.println("Missing date is " + tmp);
+                            list.add(tmp);
+                            c.add(Calendar.DATE,1);
+                        }
+                    }
+                } catch (ParseException e) {
+                    System.out.println("Error in parsing dates");
+                }
+            }
+            listHashMap.put(s, list);
+        });
+
         System.out.println("Hotels are " + hotelsID.size());
         System.out.println("Uniq hotels are " + longs.size());
-        List<String> value = usersDF.selectExpr("CAST(srch_ci AS STRING)").where("hotel_id=" + hotelsID.get(1)).orderBy("srch_ci").as(Encoders.STRING()).collectAsList();
         System.out.println("Searched val " + hotelsID.get(1));
-        System.out.println("Get val size " + value.size());
-        for(String val : value){
-            System.out.println(val);
-        }
         System.out.println("Select all ");
     }
 }
