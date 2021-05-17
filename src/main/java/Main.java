f6cd6629eb4ddd21a682db327b54c763c5ce2189
uladzimirAAsanko <volodyasn@gmail.com>
2021-05-17 12:04:54 +0300

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,6 +20,6 @@ public class Main {
             System.out.println("Part is     " + part);
         }
         HashSet<String> hotels_id = new HashSet();
-        usersDF.selectExpr("CAST(hotel_id AS STRING)").show();
+        usersDF.selectExpr("CAST(hotel_id AS LONG)").show();
     }
 }
