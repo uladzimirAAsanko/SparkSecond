050dad6d5f41f36432620281bf23fa6f49706546
uladzimirAAsanko <volodyasn@gmail.com>
2021-05-17 12:12:23 +0300

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,8 +19,8 @@ public class Main {
         for(String part : strings){
             System.out.println("Part is     " + part);
         }
-        HashSet<Long> hotels_id = new HashSet();
-        usersDF.selectExpr("CAST(hotel_id AS LONG)").foreach((ForeachFunction<Row>) row -> hotels_id.add(Long.parseLong(row.toString())));
+        HashSet<Long> hotels_id = new HashSet();//Long.parseLong(row.toString()))
+        usersDF.selectExpr("CAST(hotel_id AS LONG)").foreach((ForeachFunction<Row>) row -> hotels_id.add(row.getLong(0)));
         System.out.println("Hotels are " + (long) hotels_id.size());
     }
 }
